61a1ecb161ae65f2717e0f4a598d95992302dc90
Josué Iswa <iswajosuepro@gmail.com>
2024-09-06 10:40:29 +0100

    pro test

--- src/components/partnerslist.tsx
+++ src/components/partnerslist.tsx
@@ -3,14 +3,14 @@ import PartnerCard from "./partnercard";
 function PartnersList() {
     return (
         <div className="flex flex-row gap-4 flex-wrap mt-10 max-w-3xl p-5 items-center justify-center">
-            <PartnerCard name="Google" logo='src/assets/img/partners/google.svg' />
-            <PartnerCard name="Amazon" logo='src/assets/img/partners/amazon.svg' />
-            <PartnerCard name="Microsoft" logo='src/assets/img/partners/microsoft.svg' />
-            <PartnerCard name="Uber" logo='src/assets/img/partners/uber.svg' />
-            <PartnerCard name="Dropbox" logo='src/assets/img/partners/dropbox.svg' />
-            <PartnerCard name="Amazon" logo='src/assets/img/partners/amazon.svg' />
-            <PartnerCard name="Uber" logo='src/assets/img/partners/uber.svg' />
-            <PartnerCard name="Google" logo='src/assets/img/partners/google.svg' />
+            <PartnerCard name="Google" logo='img/partners/google.svg' />
+            <PartnerCard name="Amazon" logo='img/partners/amazon.svg' />
+            <PartnerCard name="Microsoft" logo='img/partners/microsoft.svg' />
+            <PartnerCard name="Uber" logo='img/partners/uber.svg' />
+            <PartnerCard name="Dropbox" logo='img/partners/dropbox.svg' />
+            <PartnerCard name="Amazon" logo='img/partners/amazon.svg' />
+            <PartnerCard name="Uber" logo='img/partners/uber.svg' />
+            <PartnerCard name="Google" logo='img/partners/google.svg' />
         </div>
     )
 }
